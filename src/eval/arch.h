3b13d3704ac91f23c8b897e8c497cda9e6c4f000
sscg13 <iamdumbybear1@gmail.com>
2026-07-08 13:36:39 -0700

    higher eval scale
    bench: 13866283

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -48,7 +48,7 @@ namespace oranj::eval {
     constexpr bool kDualActivation = false;
     constexpr bool kSkipL2 = false;
 
-    constexpr i32 kScale = 400;
+    constexpr i32 kScale = 550;
 
     using PsqFeatureSet = nnue::features::psq::KingBucketsMirrored<
         nnue::features::psq::MirroredKingSide::kAbcd,
